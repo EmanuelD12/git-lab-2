99ebd4d0df60ccbdeb15d7dbec1e58ee41b103e3
Emanuel <ed120720@ohio.edu>
2021-09-03 17:48:03 -0400

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Emanuel Douglas 
+ *        Date: 9/3/2021
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
